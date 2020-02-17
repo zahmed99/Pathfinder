81f9a2a82c16b946aba40fe94c6fba3885ff81ef
Zakariyah Ahmed <zakahmed68@gmail.com>
2020-02-17 16:46:35 -0500

    Added private variables

--- pathfinder.java
+++ pathfinder.java
@@ -1,8 +1,15 @@
+import java.util;
+import javax.swing;
+import java.lang;
+
 class Pathfinder {
 
 
+    private int xpos;
+    private int ypos;
+
     public static void main(String[] args) {
-        
+
     }
 
 
