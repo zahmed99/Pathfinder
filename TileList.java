25c6b1fd3bddfbfb3612c0d71156df95ef316996
Zakariyah Ahmed <zakahmed68@gmail.com>
2020-03-16 15:05:18 -0400

    Created new TileList class, which will abstract any list methods and make the Pathfinder class less complex.

---
+++ TileList.java
@@ -0,0 +1,60 @@
+import java.util.LinkedList;
+
+class TileList {
+    private LinkedList<Tile> list = new LinkedList<Tile>();
+
+    /** 
+     * Checks to see if a certain tile is in the calling list
+     * @param t The tile being checked for
+     * @return True if the tile is in the  list
+     */
+    public boolean inList(Tile tile) {
+        for (Tile t : list) {
+            if(tile.compareTo(t) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addTile(Tile t) {
+        if(this.inList(t)) return;
+        else {
+            list.add(t);
+        }
+    }
+
+    public void removeTile(Tile t) {
+        list.remove(t);
+    }
+
+    
+    /** 
+     * Iterates through the list, and gets the tile with the lowest F costs
+     * @return Tile
+     */
+    public Tile getLowestCost() {
+        Tile temp = list.getFirst();
+        for (Tile t : list) {
+            if (temp.getCost() > t.getCost()) {
+                temp = t;
+            }
+        }
+        return temp;
+    }
+
+    /**
+     * Returns a string representation of the list
+     * @param list
+     * @return
+     */
+    public String listToString() {
+        StringBuilder str = new StringBuilder();
+        String format = "";
+        for (Tile t : list) {
+            format = String.format("%4d,%d", t.getX(), t.getY());
+            str.append(format + " ");
+        }
+        return str.toString();
+    }
+}
--- pathfinder.java
+++ pathfinder.java
@@ -1,6 +1,11 @@
 import java.util.LinkedList;
 import java.util.Scanner;
 
+/* To Do:
+Create add method that checks if item is already in list
+Encapsulates list method in a custom linked list class: TileList
+*/
+
 /**
  * 
  */
@@ -10,11 +15,11 @@ class Pathfinder {
     /** Horizontal position of the pathfinder */
     private int yPos = 0;
     /** List that holds the optimal path to the end node */
-    private LinkedList<Tile> path = null;
+    private TileList path = new TileList();
     /** List of tiles currently available for evaluation */
-    private LinkedList<Tile> openList = null; 
+    private TileList openList = new TileList(); 
     /** List of tiles that are not available for evaluation */
-    private LinkedList<Tile> closedList = null; 
+    private TileList closedList = new TileList();
     /** The matrix of nodes that the pathfinder will traverse */
     private Grid grid = null;
     /** The starting node */
@@ -79,33 +84,8 @@ class Pathfinder {
      */
     public void setEnd(int x, int y) {end = grid.get(x, y);}
 
-    /** 
-     * Checks to see if a certain tile is in the open list
-     * @param t The tile being checked for
-     * @return True if the tile is in the open list
-     */
-    public boolean inOpenList(Tile t) {
-        for (Tile i : openList) {
-            if (t.compareTo(i) == 0) {
-                return true;
-            }
-        }
-        return false;
-    }
 
-    /** 
-     * Checks to see if a certain tile is in the closed list
-     * @param t The tile being checked for
-     * @return True if the tile is in the closed list
-     */
-    public boolean inClosedList(Tile t) {
-        for (Tile i : closedList) {
-            if (t.compareTo(i) == 0) {
-                return true;
-            }
-        }
-        return false;
-    }
+   
 
     /**
      * Gets user input for important variables:
@@ -117,18 +97,23 @@ class Pathfinder {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the size of the grid");
         int size = input.nextInt();
+        input.nextLine();
         grid = new Grid(size);
         
+        //Get the starting node, and add it to the closed list.
         System.out.println("Enter the x and y position of the starting tile.");
         int x = input.nextInt();
         int y = input.nextInt();
+        input.nextLine();
         start = grid.get(x, y);
         current = start;
-        openList.add(start);
+        closedList.addTile(start);
 
+        //Get the ending node
         System.out.println("Enter the x and y position of the ending tile.");
         x = input.nextInt();
         y = input.nextInt();
+        input.nextLine();
         end = grid.get(x, y);
 
         input.close();
@@ -139,28 +124,62 @@ class Pathfinder {
      * based on the movement cost.
      */
     public void findNext() {
+        Tile temp = null;
         int x = current.getX();
         int y = current.getY();
 
+        // Calculate f-costs for each neighbor, and add them to the open list
+        // if they are not already in the open list.
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                if (!this.inOpenList(grid.get(x + j, y + i))) {
-                    openList.add(grid.get(x + j, y + i));
+                try {
+                    temp = grid.get(x + j, y + i);
+                    temp.calculateCost(current, end);
+                    if (!(closedList.inList(temp))) {
+                        openList.addTile(temp);
+                    }
+                    if (temp.wall()){
+                        closedList.addTile(temp);
+                    }
+                }
+                catch(ArrayIndexOutOfBoundsException e){
+                    continue;
                 }
             }
+        } 
+
+        //Find the lowest fcost in the open list, and move pathfinder to that node
+        //add pathfinder's last position to the closed list.
+        closedList.addTile(current);
+        path.addTile(current);
+        current = openList.getLowestCost();
+        openList.removeTile(current);
+        closedList.addTile(current);
+        
+        System.out.println("Any key to continue... ");
+        try {
+            System.in.read();
         }
+        catch (Exception e) {}
     }
 
-    
     /** 
-     * @param args
+     * Main Method for testing
+     * @param args Command Line Arguments Unused
      */
     public static void main(String[] args) {
         Pathfinder A = new Pathfinder();
         A.setup();
-        if (A.current.compareTo(A.end) == 0) {
-            System.out.println(A.path);
+
+        while (A.current.compareTo(A.end) != 0) {
+            A.findNext();
         }
+
+        A.path.addTile(A.end);
+
         A.grid.printGrid();
+        System.out.println("Open List: " + A.openList.listToString());
+        System.out.println("Closed List: " + A.closedList.listToString());
+        System.out.println("Path: " + A.path.listToString());
     }
 }
